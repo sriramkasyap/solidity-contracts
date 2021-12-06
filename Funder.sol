4601cbb3feab00beab151be482640407619bbda5
Sriram Kasyap Meduri <sriramkasyap.m.s@gmail.com>
2021-12-06 07:14:45 +0530

    Funder contract

---
+++ Funder.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.8.6;
+
+contract FundingHelper {
+
+    struct Recipient {
+        address recipientAddress;
+        uint receivable;
+    }
+
+    function balance() public view returns(uint){
+        return address(this).balance;
+    }    
+}
+
+contract Funder is FundingHelper {
+    function fund(Recipient[] calldata _recipients) public payable  {
+        for(uint i = 0; i < _recipients.length; i++) {
+            require(balance() >= _recipients[i].receivable, "Insufficient Balance");
+            address payable receiver = payable(_recipients[i].recipientAddress);
+            receiver.transfer(_recipients[i].receivable);
+        }
+        address payable sender = payable(msg.sender);
+        sender.transfer(balance());
+    }
+}
